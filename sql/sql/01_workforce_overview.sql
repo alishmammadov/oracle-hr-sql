b42932e104a9de30525d0d91bc20bd90f1f29fb9
alishmammadov <145443619+alishmammadov@users.noreply.github.com>
2026-07-12 01:16:14 +0400

    Add Phase 1 SQL file

---
+++ sql/sql/01_workforce_overview.sql
@@ -0,0 +1,20 @@
+/*
+====================================================
+Project : Oracle HR SQL Portfolio
+Phase   : 1
+File    : 01_workforce_overview.sql
+
+Description:
+Workforce Overview Analysis
+
+Database:
+Oracle HR Sample Schema
+
+Tables:
+- Employees
+- Departments
+
+Author:
+Alish Mammadov
+====================================================
+*/
