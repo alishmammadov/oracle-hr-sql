0d32eeb3726b71ad87ce27623e9faf45fc8edcd9
alishmammadov <145443619+alishmammadov@users.noreply.github.com>
2026-07-12 03:38:10 +0400

    Add Phase 1 SQL file

--- sql/01_workforce_overview.sql
+++ sql/01_workforce_overview.sql
@@ -15,3 +15,54 @@ Tables:
 - Departments
 ====================================================
 */
+
+-- Task 1
+-- What is the total number of employees in the company?
+
+select count(*) as "İşçi sayı" from employees;
+
+
+-- Task 2
+-- What is the number of employees in each department?
+
+select
+department_name, count(*) as "İşci sayı"
+from departments d inner join employees e
+on d.department_id = e.department_id
+group by department_name
+order by "İşci sayı";
+
+
+-- Task 3
+-- What is the average salary by department?
+
+select
+department_id,
+round(avg(salary),2) as "Orta maaş"
+from employees
+group by department_id
+order by department_id;
+
+
+-- Task 4
+-- Show the number of employees with and without commission.
+
+select
+nvl2(commission_pct, 'Komissiya var', 'Komissiya yoxdur') as "Komissiya",
+count(*) as say
+from employees
+group by nvl2(commission_pct, 'Komissiya var', 'Komissiya yoxdur')
+
+
+-- Task 5
+-- Show each employee’s name, salary, and the number of employees in their department.
+
+select 
+first_name, 
+last_name, 
+salary, 
+count(*) over(partition by department_id) as "İşçi sayı"
+from employees
+order by "İşçi sayı";
+
+
