cba9a11e78782241550aa89866c8cc73cff4761b
alishmammadov <145443619+alishmammadov@users.noreply.github.com>
2026-07-12 01:22:07 +0400

    Add Phase 2 SQL file

---
+++ sql/sql/sql/02_salary_analysis.sql
@@ -0,0 +1,20 @@
+/*
+====================================================
+Project : Oracle HR SQL Portfolio
+Phase   : 2
+File    : 02_salary_analysis.sql
+
+Description:
+Salary Analysis
+
+Database:
+Oracle HR Sample Schema
+
+Tables:
+- Employees
+- Departments
+
+Author:
+Alish Mammadov
+====================================================
+*/
